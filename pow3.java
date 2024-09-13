7fbb5b6a8b52a53c26943eb7e4293c14be1a35c8
Aldous Roy <130863769+Aldous-Roy@users.noreply.github.com>
2024-09-13 22:28:26 +0530

    the best way

--- pow3.java
+++ pow3.java
@@ -1,16 +1,18 @@
 public class pow3 {
     public static boolean isPowerOfThree(int n) {
-        if (n <= 0) {
+        if(n==1){
+            return true; 
+        }
+        else if(n%3!=0 || n==0){
             return false;
         }
-        while (n % 3 == 0) {
-            n /= 3;
+        else{
+            return isPowerOfThree(n/3);
         }
-        return n == 1;
     }
     public static void main(String[] args) {
         System.out.println(isPowerOfThree(27));  // True, 
         System.out.println(isPowerOfThree(9));   // True, 
-        System.out.println(isPowerOfThree(10));  // False,
+        System.out.println(isPowerOfThree(0));  // False,
     }
 }
